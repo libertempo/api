da0b9afbda424a0d18339fd41454b9a82abaaefe
Prytoegrian <prytoegrian@protonmail.com>
2017-02-27 23:12:01 +0100

    Suppression constantes inutiles

--- Public/index.php
+++ Public/index.php
@@ -8,19 +8,16 @@ use Psr\Http\Message\ResponseInterface as IResponse;
 
 define('ROOT_PATH', dirname(__DIR__) . '/');
 define('CONFIG_PATH', ROOT_PATH . 'cfg/');
-define('API_PATH', ROOT_PATH . '/');
 
-define('ROUTE_PATH', API_PATH . 'Route/');
+define('ROUTE_PATH', ROOT_PATH . 'Route/');
 
-/* Virer cette cochonnerie dès que possible */
-define('_PHP_CONGES', 1);
 require_once ROOT_PATH . 'Vendor/autoload.php';
 $container = [];
-require_once API_PATH . 'Handlers.php';
+require_once ROOT_PATH . 'Handlers.php';
 
 $app = new \Slim\App($container);
 
-require_once API_PATH . 'Middlewares.php';
+require_once ROOT_PATH . 'Middlewares.php';
 
 $app->get('/hello_world', function(IRequest $request, IResponse $response) {
     $response->withJson('Hi there !');
