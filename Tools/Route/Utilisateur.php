a81cf39cc8231c075e719ba30a08f79dd8f929f3
Prytoegrian <prytoegrian@protonmail.com>
2018-01-11 22:10:51 +0100

    « Utilisateur » route access

--- Tools/Route/Utilisateur.php
+++ Tools/Route/Utilisateur.php
@@ -2,11 +2,11 @@
 /*
  * Doit être importé après la création de $app. Ne créé rien.
  *
- * La convention de nommage est de mettre les routes au pluriel
+ * La convention de nommage est de mettre les routes au singulier
  */
 
 /* Routes sur l'utilisateur et associés */
-$app->group('/utilisateurs', function () {
+$app->group('/utilisateur', function () {
     $this->group('/{utilisateurId:[0-9]+}', function () {
         /* Detail */
         $this->get('', 'controller:get')->setName('getUtilisateurDetail');
