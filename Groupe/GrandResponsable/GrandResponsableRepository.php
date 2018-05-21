7f67d36d6cd9bbb5c3b1f960246aa5711223aa97
Prytoegrian <Prytoegrian@users.noreply.github.com>
2018-05-21 16:23:13 +0200

    Ajout de la route pour le grand responsables de groupe (#37)
    * Ajout route grand_responsable
    
    * Documentation

---
+++ Groupe/GrandResponsable/GrandResponsableRepository.php
@@ -0,0 +1,73 @@
+<?php declare(strict_types = 1);
+namespace LibertAPI\Groupe\GrandResponsable;
+
+use LibertAPI\Tools\Libraries\AEntite;
+
+/**
+ * {@inheritDoc}
+ *
+ * @author Prytoegrian <prytoegrian@protonmail.com>
+ * @author Wouldsmina
+ *
+ * @since 1.0
+ * @see \LibertAPI\Tests\Units\Groupe\GrandResponsable\GrandResponsableRepository
+ */
+class GrandResponsableRepository extends \LibertAPI\Tools\Libraries\ARepository
+{
+    /*************************************************
+     * GET
+     *************************************************/
+
+    /**
+     * @inheritDoc
+     */
+    public function getOne(int $id) : AEntite
+    {
+        throw new \RuntimeException('#' . $id . ' is not a callable resource');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final protected function getParamsConsumer2Dao(array $paramsConsumer) : array
+    {
+        unset($paramsConsumer);
+        return [];
+    }
+
+    /*************************************************
+     * POST
+     *************************************************/
+
+    /**
+     * @inheritDoc
+     */
+    public function postOne(array $data, AEntite $entite)
+    {
+        throw new \RuntimeException('Action is forbidden');
+    }
+
+    /*************************************************
+     * PUT
+     *************************************************/
+
+    /**
+     * @inheritDoc
+     */
+    public function putOne(array $data, AEntite $entite)
+    {
+        throw new \RuntimeException('Action is forbidden');
+    }
+
+    /*************************************************
+     * DELETE
+     *************************************************/
+
+    /**
+     * @inheritDoc
+     */
+    public function deleteOne(AEntite $entite)
+    {
+        throw new \RuntimeException('Action is forbidden');
+    }
+}
