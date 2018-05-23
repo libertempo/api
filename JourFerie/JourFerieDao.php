4d5437fe6a358f9bc559637aad032f15c42febf2
Prytoegrian <prytoegrian@protonmail.com>
2018-05-24 00:33:50 +0200

    Moved JourFerieDao class into Jour FerieRepo class

--- JourFerie/JourFerieDao.php
+++ JourFerie/JourFerieDao.php
@@ -16,101 +16,6 @@ use LibertAPI\Tools\Libraries\AEntite;
  */
 class JourFerieDao extends \LibertAPI\Tools\Libraries\ADao
 {
-    /*************************************************
-     * GET
-     *************************************************/
-
-    /**
-     * @inheritDoc
-     */
-    public function getById(int $id) : AEntite
-    {
-        throw new \RuntimeException('Action is forbidden');
-    }
-
-    /**
-     * @inheritDoc
-     * @TODO : cette méthode le montre, JourFerie n'est pas une entité, mais un value object.
-     * L'id n'est donc pas nécessaire, et l'arbo habituelle est remise en cause
-     */
-    final protected function getStorage2Entite(array $dataDao)
-    {
-        return [
-            'id' => uniqid(),
-            'date' => $dataDao['jf_date'],
-        ];
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function getList(array $parametres) : array
-    {
-        $this->queryBuilder->select('*');
-        $this->setWhere($parametres);
-        $res = $this->queryBuilder->execute();
-
-        $data = $res->fetchAll(\PDO::FETCH_ASSOC);
-        if (empty($data)) {
-            throw new \UnexpectedValueException('No resource match with these parameters');
-        }
-
-        $entites = array_map(
-            function ($value) {
-                return new JourFerieEntite($this->getStorage2Entite($value));
-            },
-            $data
-        );
-
-        return $entites;
-    }
-
-    /*************************************************
-     * POST
-     *************************************************/
-
-    /**
-     * @inheritDoc
-     */
-    public function post(AEntite $entite) : int
-    {
-        throw new \RuntimeException('Action is forbidden');
-    }
-
-    /*************************************************
-     * PUT
-     *************************************************/
-
-    /**
-     * @inheritDoc
-     */
-    public function put(AEntite $entite)
-    {
-        throw new \RuntimeException('Action is forbidden');
-    }
-
-    /**
-     * @inheritDoc
-     */
-    final protected function getEntite2Storage(AEntite $entite) : array
-    {
-        return [
-            'jf_date' => $entite->getDate(),
-        ];
-    }
-
-    /*************************************************
-     * DELETE
-     *************************************************/
-
-    /**
-     * @inheritDoc
-     */
-    public function delete(int $id) : int
-    {
-        throw new \RuntimeException('Action is forbidden');
-    }
-
     /**
      * Définit les filtres à appliquer à la requête
      *
@@ -120,13 +25,5 @@ class JourFerieDao extends \LibertAPI\Tools\Libraries\ADao
     private function setWhere(array $parametres)
     {
         unset($parametres);
-    }
-
-    /**
-     * @inheritDoc
-     */
-    final protected function getTableName() : string
-    {
-        return 'conges_jours_feries';
     }
 }
--- JourFerie/JourFerieRepository.php
+++ JourFerie/JourFerieRepository.php
@@ -12,7 +12,7 @@ use LibertAPI\Tools\Libraries\AEntite;
  * @since 1.0
  *
  * Ne devrait être contacté que par le JourFerieController
- * Ne devrait contacter que le JourFerieEntite, JourFerieDao
+ * Ne devrait contacter que le JourFerieEntite
  */
 class JourFerieRepository extends \LibertAPI\Tools\Libraries\ARepository
 {
@@ -31,12 +31,74 @@ class JourFerieRepository extends \LibertAPI\Tools\Libraries\ARepository
     /**
      * @inheritDoc
      */
-    final protected function getParamsConsumer2Dao(array $paramsConsumer) : array
+    final protected function getParamsConsumer2Storage(array $paramsConsumer) : array
     {
         unset($paramsConsumer);
         return [];
     }
 
+    /**
+     * @inheritDoc
+     * @TODO : cette méthode le montre, JourFerie n'est pas une entité, mais un value object.
+     * L'id n'est donc pas nécessaire, et l'arbo habituelle est remise en cause
+     */
+    final protected function getStorage2Entite(array $dataStorage)
+    {
+        return [
+            'id' => uniqid(),
+            'date' => $dataStorage['jf_date'],
+        ];
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function _getList(array $parametres) : array
+    {
+        $this->queryBuilder->select('*');
+        $this->setWhere($parametres);
+        $res = $this->queryBuilder->execute();
+
+        $data = $res->fetchAll(\PDO::FETCH_ASSOC);
+        if (empty($data)) {
+            throw new \UnexpectedValueException('No resource match with these parameters');
+        }
+
+        $entites = array_map(
+            function ($value) {
+                return new JourFerieEntite($this->getStorage2Entite($value));
+            },
+            $data
+        );
+
+        return $entites;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function _post(AEntite $entite) : int
+    {
+        throw new \RuntimeException('Action is forbidden');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function _put(AEntite $entite)
+    {
+        throw new \RuntimeException('Action is forbidden');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    final protected function getEntite2Storage(AEntite $entite) : array
+    {
+        return [
+            'jf_date' => $entite->getDate(),
+        ];
+    }
 
     /*************************************************
      * DELETE
@@ -47,8 +109,16 @@ class JourFerieRepository extends \LibertAPI\Tools\Libraries\ARepository
      */
     public function deleteOne(AEntite $entite)
     {
-        $this->dao->delete($entite->getId());
+        $this->_delete($entite->getId());
         $entite->reset();
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function _delete(int $id) : int
+    {
+        throw new \RuntimeException('Action is forbidden');
     }
 
     /**
