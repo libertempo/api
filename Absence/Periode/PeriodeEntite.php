59be7297681521aa5e2319ae8e50f65ad1f166d0
Prytoegrian <prytoegrian@protonmail.com>
2018-08-01 19:26:53 +0200

    TDD PeriodeEntite

---
+++ Absence/Periode/PeriodeEntite.php
@@ -0,0 +1,159 @@
+<?php declare(strict_types = 1);
+namespace LibertAPI\Absence\Periode;
+
+/**
+ * @inheritDoc
+ *
+ * @author Prytoegrian <prytoegrian@protonmail.com>
+ * @author Wouldsmina
+ *
+ * @since 1.1
+ * @see \LibertAPI\Tests\Units\Absence\Periode\PeriodeEntite
+ *
+ * num
+ */
+class PeriodeEntite extends \LibertAPI\Tools\Libraries\AEntite
+{
+    /**
+     * Retourne la donnée la plus à jour du champ login
+     */
+    public function getLogin() : string
+    {
+        return $this->getFreshData('login');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ date début
+     */
+    public function getDateDebut() : string
+    {
+        return $this->getFreshData('date_deb');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ demi journée debut
+     */
+    public function getDemiJourneeDebut() : string
+    {
+        return $this->getFreshData('demi_jour_deb');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ date fin
+     */
+    public function getDateFin() : string
+    {
+        return $this->getFreshData('date_fin');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ demi journée fin
+     */
+    public function getDemiJourneeFin() : string
+    {
+        return $this->getFreshData('demi_jour_fin');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ nombre de jours
+     */
+    public function getNombreJours() : string
+    {
+        return $this->getFreshData('nb_jours');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ commentaire
+     */
+    public function getCommentaire() : string
+    {
+        return $this->getFreshData('commentaire');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ type
+     */
+    public function getType() : string
+    {
+        return $this->getFreshData('type');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ état
+     */
+    public function getEtat() : string
+    {
+        return $this->getFreshData('etat');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ id d'édition
+     */
+    public function getEditionId() : string
+    {
+        return $this->getFreshData('edition_id');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ motif de refus
+     */
+    public function getMotifRefus() : string
+    {
+        return $this->getFreshData('motif_refus');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ date de demande
+     */
+    public function getDateDemande() : string
+    {
+        return $this->getFreshData('date_demande');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ date traitement
+     */
+    public function getDateTraitement() : string
+    {
+        return $this->getFreshData('date_traitement');
+    }
+
+    /**
+     * Retourne la donnée la plus à jour du champ id de fermeture
+     */
+    public function getFermetureId() : string
+    {
+        return $this->getFreshData('fermeture_id');
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function populate(array $data)
+    {
+        $this->setLogin($data['login']);
+
+        $erreurs = $this->getErreurs();
+        if (!empty($erreurs)) {
+            throw new \DomainException(json_encode($erreurs));
+        }
+    }
+
+    /**
+     * Tente l'insertion d'une donnée en tant que champ « login »
+     *
+     * Stocke une erreur si la donnée ne colle pas au domaine
+     *
+     * @todo
+     */
+    private function setLogin(string $login)
+    {
+        // domaine ?
+        if (empty($login)) {
+            $this->setErreur('login', 'Le champ est vide');
+            return;
+        }
+
+        $this->dataUpdated['login'] = $login;
+    }
+}
---
+++ Tests/Units/Absence/Periode/PeriodeEntite.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types = 1);
+namespace LibertAPI\Tests\Units\Absence\Periode;
+
+/**
+ * Classe de test de l'entité de période d'absence
+ *
+ * @author Prytoegrian <prytoegrian@protonmail.com>
+ * @author Wouldsmina
+ *
+ * @since 1.1
+ */
+final class PeriodeEntite extends \LibertAPI\Tests\Units\Tools\Libraries\AEntite
+{
+    /**
+     * @inheritDoc
+     */
+    public function testConstructWithId()
+    {
+        $id = 3;
+        $login = 'Logan';
+        $commentaire = 'Je ne suis pas gros !';
+
+        $this->newTestedInstance(['id' => $id, 'login' => $login, 'commentaire' => $commentaire]);
+
+        $this->assertConstructWithId($this->testedInstance, $id);
+        $this->string($this->testedInstance->getLogin())->isIdenticalTo($login);
+        $this->string($this->testedInstance->getCommentaire())->isIdenticalTo($commentaire);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function testConstructWithoutId()
+    {
+        $this->newTestedInstance(['name' => 'name', 'status' => 'status']);
+
+        $this->variable($this->testedInstance->getId())->isNull();
+    }
+
+    /**
+     * Teste la méthode populate avec un mauvais domaine de définition
+     */
+    public function testPopulateBadDomain()
+    {
+        $this->newTestedInstance([]);
+        $data = ['login' => ''];
+
+        $this->exception(function () use ($data) {
+            $this->testedInstance->populate($data);
+        })->isInstanceOf('\DomainException');
+    }
+
+    /**
+     * Teste la méthode populate avec ok
+     */
+    public function testPopulateOk()
+    {
+        $this->newTestedInstance([]);
+        $data = ['login' => 'Logan'];
+
+        $this->testedInstance->populate($data);
+
+        $this->string($this->testedInstance->getLogin())->isIdenticalTo($data['login']);
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function testReset()
+    {
+        $this->newTestedInstance(['id' => 3, 'name' => 'name', 'status' => 'status']);
+
+        $this->assertReset($this->testedInstance);
+    }
+}
